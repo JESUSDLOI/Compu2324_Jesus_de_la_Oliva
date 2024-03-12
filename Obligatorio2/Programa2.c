bfcfa5df2b10232d2c0fdd59f16f16f0a3e63dc6
JESUSDLOI <jeus.delaoliva.ig@gmailcom>
2024-03-12 17:42:28 +0100

    Programa de números aleatorios.

---
+++ Obligatorio2/Programa2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef USE_GSL
+#include <gsl/gsl_rng.h>
+#endif
+
+#define RNG_TYPE 1  // 0 for GSL, 1 for C rand
+
+int generate_random(int n, int m) {
+    int result = 0;
+    int i=0;
+
+    #if RNG_TYPE == 0
+    #ifdef USE_GSL
+    const gsl_rng_type * T;
+    gsl_rng * r;
+
+    gsl_rng_env_setup();
+
+    T = gsl_rng_default;
+    r = gsl_rng_alloc (T);
+
+    result = gsl_rng_uniform_int(r, n+1);
+    gsl_rng_free (r);
+        fopen("random_numbers.txt", "w", stdout);
+        do while (i<m, i++)
+        {
+            srand(time(0));
+            result = rand() % (n+1);
+            fprintf("random_numbers.txt", "%d\n", result);
+
+        }
+    fclose("random_numbers.txt");
+   
+    #endif
+    #elif RNG_TYPE == 1
+    FILE *file = fopen("random_numbers.txt", "w");
+    srand(time(0));
+        do
+        {   
+            result = rand() % (n+1);
+            fprintf(file, "%d\n", result);
+             i++;
+        }while (i<m);
+        fclose(file);
+   
+    #endif
+}
+
+int main() {
+    int n = 100;
+    int m = 300;
+    int random_number = generate_random(n, m);
+    return 0;
+}
