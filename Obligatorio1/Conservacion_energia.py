3c946ac91e2f9915eaf8e1e7f8fbbbd98adfc407
JESUSDLOI <jesus.delaoliva.ig@gmail.com>
2024-04-09 00:42:44 +0200

    Añado el sol a los cálculos y numba

--- Obligatorio1/Conservacion_energia.py
+++ Obligatorio1/Conservacion_energia.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-
 
 G = 6.674*10**(-11) 
 
@@ -16,17 +14,17 @@ m_saturno = 5.68*10**26
 m_urano = 8.68*10**25
 m_neptuno = 1.02*10**26
 m_pluton = 1.3*10**22
-masas = np.array([m_mercurio, m_venus, m_tierra, m_marte, m_jupiter, m_saturno, m_urano, m_neptuno, m_pluton])
+masas = np.array([m_sol, m_mercurio, m_venus, m_tierra, m_marte, m_jupiter, m_saturno, m_urano, m_neptuno, m_pluton])
 m_rees = [ m/m_sol for m in masas]
 
 #Numero de planetas
-n = 8
+n = 6
 
 # Leer las velocidades de un archivo de texto
-with open(r'C:\Users\jesol\OneDrive\Escritorio\Programacion\Programas\Compu2324\Obligatorio1\velocidades.dat', 'r') as f:
+with open('velocidades.dat', 'r') as f:
     v = [[list(map(float, line.split(','))) for line in block.split('\n') if line] for block in f.read().split('\n\n')]
 
-with open(r'C:\Users\jesol\OneDrive\Escritorio\Programacion\Programas\Compu2324\Obligatorio1\posiciones.dat', 'r') as f:
+with open('posiciones.dat', 'r') as f:
     coordenadas = [[list(map(float, line.split(','))) for line in block.split('\n') if line] for block in f.read().split('\n\n')]
     
 v.pop()
@@ -52,38 +50,31 @@ for k in range(len(coordenadas)):
         for j in range(n):
             if i != j:
                 E_pot[i][k] += (-m_rees[i]*m_rees[j])/(np.linalg.norm(np.array(coordenadas[k][i])-np.array(coordenadas[k][j])))
-        E_pot[i][k] += (-m_rees[i])/(np.linalg.norm(np.array(coordenadas[i])))
+        #E_pot[i][k] += (-m_rees[i])/(np.linalg.norm(np.array(coordenadas[i])))
                 
 # Inicializar E_planeta array
 E_planeta = np.zeros((n, int(filas)))
+# Inicializar E_total array
+E_total = np.zeros((1, int(filas)))
 
 for i in range(n):
     for j in range(int(filas)):
         E_planeta[i][j] = E_cin[i][j] + E_pot[i][j]
+        E_total[0][j] = E_planeta[i][j]
 
 
 # Graficar la energía cinética            
-labels = ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno']
-
-# Inicializar excentricidad array
-excentricidad = np.zeros(n)
-
-# Calcular la excentricidad
-for i in range(n):
-    excentricidad[i] = np.sqrt(1 + (2*E_planeta[i][0]*(m_rees[i]*np.linalg.norm(coordenadas[0][i])**3*np.linalg.norm(v[0][i]))**2)/(m_rees[i]**3))
-
-print(excentricidad)
+labels = ['Sol', 'Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno']
 
 
-for i in range(n):
-    plt.figure(i)  # Crear una nueva figura para cada conjunto de datos
+for i in range(n):  
+    plt.figure(i)
 
     # Graficar los datos
     plt.plot(E_cin[i], label='Energía cinética ' + labels[i])
     plt.plot(E_pot[i], label='Energía potencial ' + labels[i])
     plt.plot(E_planeta[i], label='Energía total ' + labels[i])
-
-
+    plt.plot(E_total[0], label='Energía total')
 
 
 # Añadir título y etiquetas
--- Obligatorio1/Simulacion_sistema_solar.py
+++ Obligatorio1/Simulacion_sistema_solar.py
@@ -2,20 +2,27 @@
 #Unidades del sistema internacional.
 
 import numpy as np
+import time
+from numba import jit
+
+#Establecemos el tiempo inicial.
+
+t0 = time.time()
 
 #Establecemos los uncrementos del tiempo.
 h=0.0001
 
 #Número de iteraciones.
-iteraciones = 1000000
+iteraciones = 10000
 
 #Pedimos el número de planetas con los que se ejcutará la simulación.
-n = 8
+n = 6
 
 #Constante de gravitación universal.
 G = 6.674*10**(-11) 
 
 #Distancia al Sol de los planetas m.
+r_sol = np.array([0, 0])
 r_mrcurio = np.array([5.791*10**10, 0])
 r_venus = np.array([1.082*10**11, 0])
 r_tierra = np.array([1.496*10**11, 0])
@@ -25,7 +32,7 @@ r_saturno = np.array([1.433*10**12, 0])
 r_urano = np.array([2.871*10**12, 0])
 r_neptuno = np.array([4.495*10**12, 0])
 r_pluton = np.array([5.906*10**12, 0])
-radios = np.array([r_mrcurio, r_venus, r_tierra, r_marte, r_jupiter, r_saturno, r_urano, r_neptuno, r_pluton])
+radios = np.array([r_sol, r_mrcurio, r_venus, r_tierra, r_marte, r_jupiter, r_saturno, r_urano, r_neptuno, r_pluton])
 
 #Masas de los planetas Kg
 m_sol = 1.989*10**30
@@ -38,9 +45,10 @@ m_saturno = 5.68*10**26
 m_urano = 8.68*10**25
 m_neptuno = 1.02*10**26
 m_pluton = 1.3*10**22
-masas = np.array([m_mercurio, m_venus, m_tierra, m_marte, m_jupiter, m_saturno, m_urano, m_neptuno, m_pluton])
+masas = np.array([m_sol, m_mercurio, m_venus, m_tierra, m_marte, m_jupiter, m_saturno, m_urano, m_neptuno, m_pluton])
 
 #Vector de velocidades iniciales de los planetas m/s.
+v_sol = np.array([0, 0])
 v_mercurio = np.array([0, -4.7*10**4])
 v_venus = np.array([0, -3.5*10**4])
 v_tierra = np.array([0, 3*10**4])
@@ -64,37 +72,40 @@ r_rees = np.array([[r, 0] for r in reescalado_r])
 m_rees = [ m/m_sol for m in masas]
 
 #Definimos la función a(t) a partir de la suma de fuerazas que se ejercen sobre cada partícula i.
+@jit
 def aceleracion_por_planeta(n, r_rees, m_rees, a_t):
     for i in range(n):
         for j in range(n):
             if i != j:
                 a_t[i] += m_rees[j]*np.array(r_rees[i] - r_rees[j])/np.linalg.norm(r_rees[i] - r_rees[j])**3  
-        a_t[i] += r_rees[i]/np.linalg.norm(r_rees[i])**3  
     return -a_t
 
 #Definimos la función w[i].
+@jit
 def w_ih(n, v_rees, a_i, w_i, h):
     for i in range(n):
         w_i[i] = v_rees[i] + a_i[i]*(h/2)
     return w_i
 
 #Definimos r(t+h) que nos da la nueva posición.
+@jit
 def r_th(n, r_rees_th, w_i, h):
     for i in range(n):
         r_rees_th[i] = r_rees_th[i] + w_i[i]*h
     return r_rees_th
 
 #Definimos la función que nos da la acceleración en el tiempo t+h.
+@jit
 def acel_i_th(n, r_rees_th, m_rees, a_i_th):
     for i in range(n):
         for j in range(n):
             if i != j:
                  a_i_th[i] += m_rees[j]*np.array(r_rees[i] - r_rees[j])/np.linalg.norm(r_rees[i] - r_rees[j])**3    
-        a_i_th[i] += r_rees_th[i]/np.linalg.norm(r_rees_th[i])**3
     return -a_i_th 
 
 
 #Definimos la función que nos da la nueva velocidad en el tiempo t+h.
+@jit
 def velocidad_th(w_i, n, v_th, a_i_th, h):
     for i in range(n):
         v_th[i]= w_i[i] + a_i_th[i]*(h/2)
@@ -110,9 +121,9 @@ a_i_th = np.zeros((n, 2))
 v_th = v_rees
 
 # Abrir tres archivos para guardar los datos de las posiciones, velocidades y aceleraciones
-file_posiciones = open("C:\\Users\\jesol\\OneDrive\\Escritorio\\Programacion\\Programas\\Compu2324\\Obligatorio1\\posiciones.dat", "w")
-file_velocidades = open("C:\\Users\\jesol\\OneDrive\\Escritorio\\Programacion\\Programas\\Compu2324\\Obligatorio1\\velocidades.dat", "w")
-file_aceleraciones = open("C:\\Users\\jesol\\OneDrive\\Escritorio\\Programacion\\Programas\\Compu2324\\Obligatorio1\\aceleraciones.dat", "w")
+file_posiciones = open('posiciones.dat', "w")
+file_velocidades = open('velocidades.dat', "w")
+file_aceleraciones = open('aceleraciones.dat', "w")
 
 # Realizamos el bucle para calcular las posiciones y velocidades de los planetas.
 for k in range(iteraciones):
@@ -133,11 +144,14 @@ for k in range(iteraciones):
     v_rees = v_th
     a_i = a_i_th
 
-print(r_rees)
-print(v_rees)
-print(a_i)
-    
 # Cerrar los archivos
 file_posiciones.close()
 file_velocidades.close()
 file_aceleraciones.close()
+
+#Tiempo final.
+t1 = time.time()
+
+tiempo = t1 - t0
+
+print("El tiempo de ejecución es: ", tiempo, "segundos")
