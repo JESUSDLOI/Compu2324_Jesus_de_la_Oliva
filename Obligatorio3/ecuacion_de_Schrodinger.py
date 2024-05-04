9e4c5b3c4f363dc95ae4eca648f73a15b38e53e4
JESUSDLOI <jesus.delaoliva.ig@gmail.com>
2024-05-04 14:43:09 +0200

    Obligatorio 3 Funcional

--- Obligatorio3/ecuacion_de_Schrodinger.py
+++ Obligatorio3/ecuacion_de_Schrodinger.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.integrate import odeint
 import time
 
 t_o = time.time()
@@ -13,7 +12,7 @@ nciclos = N/4
 λ = 0.7
 
 # Generar s, k0, Vj, Φj_0 and alpha
-k0 = 2 * np.pi / 4
+k0 = 2 * np.pi * nciclos/ N
 s = 1 / (4*k0**2)
 
 #Inicializar las variables
@@ -27,30 +26,31 @@ phi = np.zeros(N)
 
 #Calcular potencial, Vj
 def calc_Vj(N, λ, k0, Vj):
+    V2 = np.zeros(N)
     for i in range(N):
         if i >= 2*N/5 and i <= 3*N/5:
             Vj[i] = λ * k0**2
         else:
             Vj[i] = 0
+        V2[i] = abs(Vj[i])**2
     return Vj
 
 #Función onda inicial
 def onda_inicial(N, k0, Oj_0, phi):
-    for i in range(0, N-1):
-        Oj_0[i] = np.exp(-1j * k0 * i)*np.exp(-8*(4*i-N)**2 / N**2)
-        phi[i] = np.abs(Oj_0[i])**2
-    Oj_0[N-1] = 0
+    for i in range(1, N-1):
+        Oj_0[i] = np.exp(1j * k0 * i)*np.exp(-8*(4*i-N)**2 / N**2)
+        phi[i] = np.abs(Oj_0[i])**2     
     return Oj_0, phi
 
 # Calcular alpha
 def calc_alpha(s, Vj, alpha, N):
-    for i in range(N-1, 0, -1):
+    for i in range(N-2, -1, -1):
         alpha[i-1] = -1/((-2 + (2j/s) - Vj[i])+(alpha[i]))
     return alpha
 
 # Calcular beta
 def calc_beta(s, Oj_0, alpha, beta, Vj, N):
-    for i in range(N-1, 0, -1):
+    for i in range(N-2, -1, -1):
         beta[i-1] = ((4j*Oj_0[i]/s)-beta[i])/((-2 + 2j/s - Vj[i])+(alpha[i]))
     return beta
 
@@ -76,9 +76,9 @@ def funcion_onda_temporal(N, k0, Oj_0, iteraciones, s, Vj, alpha, beta, Xj_n, Oj
     alpha = calc_alpha(s, Vj, alpha, N)
 
     for j in range(iteraciones):
-        norma = sum(phi)
+        norma = np.sqrt(sum(phi))
+        phi = phi / norma
         file_2.write(f"{j}, {norma}\n")
-        Oj_0 = Oj_0 / np.sqrt(norma)
         for i in range(N):
             file.write(f"{i}, {phi[i]}, {Vj[i]}\n")
         beta = calc_beta(s, Oj_0, alpha, beta, Vj, N)
